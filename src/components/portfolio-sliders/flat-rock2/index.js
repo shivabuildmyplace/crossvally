fdd07a456c9fd33e5257d464bb7be1fe5f64a8ab
shivabuildmyplace <rudroju@wenlighting.com>
2025-04-10 01:29:39 +0530

    removed slider
    fonts improved and slider removed

--- src/components/portfolio-sliders/flat-rock2/index.js
+++ src/components/portfolio-sliders/flat-rock2/index.js
@@ -1,47 +1,40 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  A11y,
-  Autoplay,
-} from "swiper/modules";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
 import "./index.css";
 function Flatrock2() {
+  let images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div className="slider-container">
-      <div className="slider-section">
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-          autoplay={{
-            delay: 4000,
-            disableOnInteraction: false,
-          }}
-        >
-          <SwiperSlide>
-            <div className="sliderimage">
-              <img src="flatrock2.png" alt="imag1" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="sliderimage">
-              <img src="/swiper2.png" alt="img2" />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="sliderimage">
-              <img src="/swiper3.png" alt="imag1" />
-            </div>
-          </SwiperSlide>
-        </Swiper>
+      <div className="flex-container">
+        <ul>
+          {images.map((items) => (
+            <li className="pageImages">{items}</li>
+          ))}
+        </ul>
+        <div>
+          <h3>
+            A property description, at its core, is more than just a rundown of
+            a home's features and specs. It's a crafted narrative that plays a
+            crucial role in the real estate market, serving as the bridge
+            between a listing and its potential buyers. This narrative
+            transforms cold, hard facts about square footage, location, and
+            amenities into a story that resonates with home shoppers, making
+            them feel as though they've found their dream home before even
+            stepping through the front door. Creating a compelling property
+            listing description is an art that blends creativity with strategy,
+            turning a standard listing into a captivating narrative that grabs
+            potential buyers' attention and gets clicks. It's about highlighting
+            unique features, like a sun-filled master bedroom overlooking a
+            tree-lined street or a gourmet kitchen designed for entertaining,
+            coupled with attention-grabbing headlines, brand name appliances,
+            and SEO-boosting keywords. These elements are crucial in crafting
+            descriptions that not only stand out but resonate deeply with
+            potential buyers, encouraging them to envision their future in the
+            home. Every word, from describing a cozy fireplace to a spa-like
+            bathroom, is a chance to connect, persuade, and ultimately, convert
+            interest into action. This strategic approach transforms a real
+            estate listing description from mere details into a compelling
+            invitation to a potential
+          </h3>
+        </div>
       </div>
     </div>
   );
