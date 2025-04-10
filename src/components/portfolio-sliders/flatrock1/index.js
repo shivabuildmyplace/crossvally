3c7065dec3f75a2cb4c7f63c152bea59ccf6f11f
shivabuildmyplace <rudroju@wenlighting.com>
2025-04-11 02:19:45 +0530

    images
    added  images

--- src/components/portfolio-sliders/flatrock1/index.js
+++ src/components/portfolio-sliders/flatrock1/index.js
@@ -1,7 +1,13 @@
 import "./index.css";
 
 function Flatrock1() {
-  let images = ["img1.png", "img2.png", "img3.png"];
+  let images = [
+    "img11.png",
+    "img12.png",
+    "img13.png",
+    "img14.png",
+    "img15.png",
+  ];
   return (
     <div className="slider-container">
       <h5>Flatrock</h5>
