28299338f9acffe834c0e0cb444d2e6a9d6419ee
shivabuildmyplace <rudroju@wenlighting.com>
2025-04-18 20:59:52 +0530

    removed swiper

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,50 +1,20 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import {
-  Navigation,
-  Scrollbar,
-  A11y,
-  Autoplay,
-  EffectFade,
-} from "swiper/modules";
-import "swiper/css";
-import "swiper/css/scrollbar";
-import "swiper/css/effect-fade";
 import "./home.css";
 
 function Home() {
   return (
     <div className="home-swiper">
-      <Swiper
-        modules={[Navigation, Scrollbar, A11y, Autoplay, EffectFade]}
-        effect="fade"
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        pagination={{ clickable: true }}
-        style={{
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        {[...Array(1)].map((_, index) => (
-          <SwiperSlide key={index}>
-            <div className="sliderimage2">
-              <img
-                loading="eager"
-                // src={`/bisely4${index + 1}.jpg`}
-                src="/bisely4.jpg"
-                alt={`Slide ${index + 1}`}
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-              />
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="sliderimage2">
+        <img
+          loading="eager"
+          src="/bisely4.jpg"
+          alt="Slide 1"
+          style={{
+            width: "100vw",
+            height: "100vh",
+            objectFit: "cover",
+          }}
+        />
+      </div>
     </div>
   );
 }
